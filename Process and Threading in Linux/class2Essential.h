77b241f7acc81c42b81ae1a6232c4e1e0827d926
Willibrordus Bayu <87259917+Bayunova28@users.noreply.github.com>
2021-12-08 22:47:47 +0700

    added praktikum

---
+++ Process and Threading in Linux/class2Essential.h
@@ -0,0 +1,13 @@
+#include <string>
+
+#ifndef class2Essential_H
+#define class2Essential_H
+
+class class2Essential {
+	private:
+		int hitungPrima(int limitAtas);
+	public:
+		void cekPrima(int limitAtas, const std::string tipe);
+};
+
+#endif
