440822bdc0749f1cf31d81eba5c09f7b742f3d5b
Willibrordus Bayu <87259917+Bayunova28@users.noreply.github.com>
2021-12-08 22:35:55 +0700

    added praktikum

---
+++ Introduction to Ubuntu Server/Praktikum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout<<"Hello World!/n";
+  return 0;
+}
+
+class lab1{
+  public:
+    void helloeWorld(){
+      cout<<"Hello World!\n";
+    }
+};
+
+int main(){
+  lab1 jalankanHelloWorld;
+  jalankanHellWorld.....;
+  return 0;
+}
